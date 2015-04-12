906cf1cfaa6ba9b4cbabaa4f482df8d323ccf089
pennybear1009 <pei.xiaoning@gmail.com>
2015-04-12 22:19:12 +0800

    essage

--- ExData_Plotting1/plot3.R
+++ ExData_Plotting1/plot3.R
@@ -12,7 +12,7 @@ plot(dat$DT,dat$Sub_metering_1, type="n", xlab="", ylab="Energy sub metering")
 points(dat$DT,dat$Sub_metering_1, type="l", col="black")
 points(dat$DT,dat$Sub_metering_2, type="l",col="red")
 points(dat$DT,dat$Sub_metering_3, type="l",col="blue")
-legend("topright", pch="-", col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+legend("topright", lty=c(1,1,1), col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
 dev.off()
 
 
